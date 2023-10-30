4423c9770b00ee74210fb2953bc9730de3e0090a
shubhamchavan1 <95968002+ShubhamChavan1@users.noreply.github.com>
2023-10-30 16:37:10 +0530

    Update arduinocode.ino

--- arduinocode/arduinocode.ino
+++ arduinocode/arduinocode.ino
@@ -1,16 +1,18 @@
-
-
-int IrSenor = 12;
+int IrSensor = 12;
 int Buzzer = 7;
+
 void setup() {
- Serial.begin(9600);
- pinMode{irSensor, INPUT};
- pinMode{buzzer, OUTPUT};
+  Serial.begin(9600);
+  pinMode(IrSensor, INPUT);
+  pinMode(Buzzer, OUTPUT);
 }
+
 void loop() {
- int value = digitalRead(irSensor):
- Serial.println("");
- Serial.print("Sensor Value =");
- Serial.print(value);
- delay(1000);
+  int value = digitalRead(IrSensor);
+  Serial.println("");
+  Serial.print("Sensor Value = ");
+  Serial.print(value);
+  delay(1000);
 }
+
+
